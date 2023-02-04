42ace6c170f816cc41fe4a27ff78438c7fd893b0
frahz <abreufranklin589@gmail.com>
2023-02-04 00:30:31 -0800

    fix: fix json parsing error and add error handling for reqwest response

--- src/commands/wotd.rs
+++ src/commands/wotd.rs
@@ -1,4 +1,3 @@
-// use serde::Deserialize;
 use chrono::prelude::*;
 use rand::seq::SliceRandom;
 use std::env;
@@ -45,28 +44,39 @@ struct Example {
     url: String,
     title: String,
     text: String,
-    id: u32,
+    id: Option<u32>,
 }
 
 #[command]
 pub async fn word(ctx: &Context, msg: &Message) -> CommandResult {
+    info!("Sending Word of the Day");
     let wordnik_api = env::var("WORDNIK_API_KEY").expect("wordnik api key");
     let url = format!("https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key={}",wordnik_api);
 
     let dt = Utc::now().format("%B %d, %Y");
     let color = utils::COLORS.choose(&mut rand::thread_rng()).unwrap();
 
-    let res: Response = reqwest::get(url).await?.json::<Response>().await?;
+    let res = match reqwest::get(url).await?.json::<Response>().await {
+        Ok(r) => r,
+        Err(e) => {
+            error!("Problem parsing JSON: {:?}", e);
+            msg.channel_id.say(&ctx.http, "had a problem parsing JSON!").await?;
+            return Ok(());
+        },
+    };
+
     let example = res.examples[0].text.replace(&res.word, &format!("**{}**", res.word));
+
     msg.channel_id
         .send_message(&ctx.http, |m| {
             m.embed(|e| {
                 e.title(format!("{} | {}", res.word, dt))
                     .colour(*color)
-                    .field("Pronounciation", res.publish_date, false)
+                    // .field("Pronounciation", res.publish_date, false)
                     .field("Word type", format!("*{}*", res.definitions[0].part_of_speech), false)
                     .field("Definition", &res.definitions[0].text, false)
                     .field("Example", example, false)
+                    .field("Note", res.note, false)
                     .footer(|f| {
                         f.text("Word of the Day");
                         f
