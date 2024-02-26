08c32057368374d2d505864026e2e749d2538eaa
frahz <abreufranklin589@gmail.com>
2024-02-25 16:15:02 -0800

    add default package to flake

--- flake.nix
+++ flake.nix
@@ -31,9 +31,15 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
-        nativeBuildInputs = with pkgs; [libopus pkg-config];
-        buildInputs = with pkgs; [rust-bin.stable.latest.default yt-dlp openssl];
+        nativeBuildInputs = with pkgs; [pkg-config];
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+          yt-dlp
+          openssl
+          libopus
+        ];
       in
         with pkgs; {
           checks = {
@@ -41,6 +47,17 @@
               src = ./.;
               hooks = {
                 rustfmt.enable = true;
+              };
+            };
+          };
+          packages.default = rustPlatform.buildRustPackage {
+            inherit nativeBuildInputs buildInputs;
+            inherit (cargoToml.package) name version;
+            src = lib.cleanSource ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "songbird-0.4.0" = "sha256-lrf19DxuuzGcqLLLfMfI/dC/TdjKMMgxPZXgPxoxBsA=";
               };
             };
           };
