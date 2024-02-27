282a676213fccb6511aea250c1d541cf4d0f8f45
frahz <abreufranklin589@gmail.com>
2024-02-26 23:40:06 -0800

    fix nixos module

--- flake.nix
+++ flake.nix
@@ -65,46 +65,46 @@
             inherit (self.checks.${system}.pre-commit-check) shellHook;
             inherit nativeBuildInputs buildInputs;
           };
-          nixosModules.default = {
-            config,
-            lib,
-          }:
-            with lib; let
-              cfg = config.services.raulyrs;
-            in {
-              options.services.raulyrs = {
-                enable = mkEnableOption "rauly.rs discord bot";
-                package = mkOption {
-                  type = types.package;
-                  default = self.packages.default;
-                  description = ''
-                    Package for rauly.rs discord bot
-                  '';
-                };
-                environmentFile = mkOption {
-                  type = types.path;
-                  description = ''
-                    Path containing the Bot's API keys
-                  '';
-                };
-              };
-              config = mkIf cfg.enable {
-                systemd.services.raulyrs = {
-                  description = "rauly.rs discord bot";
-                  wantedBy = ["multi-user.target"];
-                  serviceConfig = {
-                    Type = "Simple";
-                    ExecStart = lib.getExe cfg.package;
-                    EnvironmentFile = cfg.environmentFile;
-                  };
-                };
-              };
-            };
         }
     )
     // {
       overlays.default = final: _: {
         raulyrs = final.callPackage self.packages.${final.system}.default {};
       };
+      nixosModules.default = {
+        config,
+        lib,
+      }:
+        with lib; let
+          cfg = config.services.raulyrs;
+        in {
+          options.services.raulyrs = {
+            enable = mkEnableOption "rauly.rs discord bot";
+            package = mkOption {
+              type = types.package;
+              default = self.packages.default;
+              description = ''
+                Package for rauly.rs discord bot
+              '';
+            };
+            environmentFile = mkOption {
+              type = types.path;
+              description = ''
+                Path containing the Bot's API keys
+              '';
+            };
+          };
+          config = mkIf cfg.enable {
+            systemd.services.raulyrs = {
+              description = "rauly.rs discord bot";
+              wantedBy = ["multi-user.target"];
+              serviceConfig = {
+                Type = "Simple";
+                ExecStart = lib.getExe cfg.package;
+                EnvironmentFile = cfg.environmentFile;
+              };
+            };
+          };
+        };
     };
 }
