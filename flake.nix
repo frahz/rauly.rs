e19e88fac0bb6cb7bb247397b9b328f671306f5b
frahz <abreufranklin589@gmail.com>
2024-05-01 21:45:59 -0700

    update flake layout

--- flake.nix
+++ flake.nix
@@ -73,53 +73,6 @@
       overlays.default = final: _: {
         raulyrs = final.callPackage self.packages.${final.system}.default {};
       };
-      nixosModules.default = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }:
-        with lib; let
-          cfg = config.services.raulyrs;
-        in {
-          options.services.raulyrs = {
-            enable = mkEnableOption "rauly.rs discord bot";
-            package = mkOption {
-              type = types.package;
-              default = pkgs.raulyrs;
-              description = ''
-                Package for rauly.rs discord bot
-              '';
-            };
-            environmentFile = mkOption {
-              type = types.path;
-              description = ''
-                Path containing the Bot's API keys
-              '';
-            };
-          };
-          config = mkIf cfg.enable {
-            systemd.services.raulyrs = {
-              description = "rauly.rs discord bot";
-              wantedBy = ["multi-user.target"];
-              path = [ pkgs.yt-dlp ];
-              serviceConfig = {
-                Type = "simple";
-                User = "raulyrs";
-                ExecStart = lib.getExe cfg.package;
-                EnvironmentFile = cfg.environmentFile;
-              };
-            };
-
-            users = {
-              users.raulyrs = {
-                description = "rauly.rs service user";
-                isSystemUser = true;
-                group = "raulyrs";
-              };
-              groups.raulyrs = {};
-            };
-          };
-        };
+      nixosModules.default = import ./nix/module.nix;
     };
 }
