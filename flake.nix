b69a86351efc8cb3b3991f8988d9de82b9b3ffd8
frahz <me@frahz.dev>
2024-12-29 16:29:18 -0800

    chore: remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
 
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
@@ -10,62 +9,46 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     rust-overlay,
     pre-commit-hooks,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [(import rust-overlay)];
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      # "aarch64-darwin"
+    ];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsForEach = forEachSystem (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [rust-overlay.overlays.default];
+      });
+  in {
+    checks = forEachSystem (system: {
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          rustfmt.enable = true;
         };
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-
-        nativeBuildInputs = with pkgs; [pkg-config];
-        buildInputs = with pkgs; [
-          rust-bin.stable.latest.default
-          yt-dlp
-          openssl
-          libopus
-        ];
-      in
-        with pkgs; {
-          checks = {
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                rustfmt.enable = true;
-              };
-            };
-          };
-          packages = {
-            default = rustPlatform.buildRustPackage {
-              inherit nativeBuildInputs buildInputs;
-              inherit (cargoToml.package) name version;
-              src = lib.cleanSource ./.;
-              cargoLock.lockFile = ./Cargo.lock;
-
-              meta = with lib; {
-                description = "rauly.rs discord bot";
-                homepage = "https://github.com/frahz/rauly.rs";
-                licenses = licenses.mit;
-                mainProgram = "raulyrs";
-              };
-            };
-            raulyrs = self.packages.${system}.default;
-          };
-          devShells.default = mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            inherit nativeBuildInputs buildInputs;
-          };
-        }
-    )
-    // {
-      overlays.default = final: _: {
-        raulyrs = final.callPackage self.packages.${final.system}.default {};
       };
-      nixosModules.default = import ./nix/module.nix self;
-    };
+    });
+
+    devShells = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+    in {
+      default = pkgs.callPackage ./nix/shell.nix {
+        inherit pkgs;
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+      };
+    });
+
+    packages = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+    in {
+      raulyrs = pkgs.callPackage ./nix/package.nix {};
+      default = self.packages.${system}.raulyrs;
+    });
+
+    nixosModules.default = import ./nix/module.nix self;
+  };
 }
